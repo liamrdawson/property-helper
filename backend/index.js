14d087c09ecb39ef4764676e4122318778789ec3
Liam Dawson <liamrdawson@gmail.com>
2019-11-19 20:11:44 +0000

    add all property values to an array as strings

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
-import { getHtml } from './lib/scraper';
+import { getHtml, getRightMoveProperty } from './lib/scraper';
+
+const rightMove =
+  'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E93554&minBedrooms=2&maxPrice=375000&radius=3.0&propertyTypes=flat&primaryDisplayPropertyType=flats&includeSSTC=false&mustHave=newHome&dontShow=retirement&furnishTypes=&keywords=';
 
 async function startScrape() {
-  console.log(await getHtml());
+  getRightMoveProperty(await getHtml(rightMove));
 }
 
 startScrape();
--- backend/lib/scraper.js
+++ backend/lib/scraper.js
@@ -1,4 +1,18 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
 export async function getHtml(url) {
-  const html = 'cool';
+  const { data: html } = await axios.get(url);
   return html;
+}
+
+export async function getRightMoveProperty(html) {
+  const $ = cheerio.load(html);
+  const span = $(`.propertyCard-priceValue`).text();
+  const modSpan = span
+    .split(' ')
+    .filter(place => place !== '')
+    .map(price => price);
+  console.log(modSpan);
+  return span;
 }
